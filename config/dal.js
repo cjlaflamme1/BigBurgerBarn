418a33e2371107ad3d729cfc2287f5608b7d268e
Chad Laflamme <cjlaflamme1@plymouth.edu>
2020-09-15 20:33:00 -0400

    updated connection, DAL, seeds, basic html

--- config/dal.js
+++ config/dal.js
@@ -1,4 +1,47 @@
-// This is the data access layer file, (ORM?)
-// require connection
-// Created methods selectAll() insertOne() updateOne() and deleteOne()
-// don't forget to export
+const connection = require('./connection');
+class DAL {
+    connection;
+
+    constructor(connection) {
+        this.connection = connection;
+    }
+
+    query = (queryString, vals) => {
+        return new Promise((resolve, reject) => {
+            this.connection.query(queryString, vals, function (err, result) {
+                if (err) {
+                    reject(err);
+                }
+                resolve(result);
+            });
+        })
+    };
+
+
+    
+    selectAll(table) {
+        return this.query(`SELECT * FROM ${table}`);
+    }
+
+
+    insertOne(table, column, burgerName) {
+        const queryString = `INSERT INTO ${table} (${column}) VALUES ( ? )`;
+        console.log(queryString);
+        return this.query(queryString, burgerName);
+    }
+
+    updateOne(table, newValues, condition) {
+        const queryString = `UPDATE ${table} SET ${newValues} WHERE ${condition}`;
+        console.log(queryString);
+        return this.query(queryString);
+    }
+
+    delete(table, condition) {
+        const queryString = `DELETE FROM ${table} WHERE ${condition}`;
+        return this.query(queryString);
+    }
+
+}
+
+
+module.exports = DAL;
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,11 +1,5 @@
-SHOW CREATE TABLE `burgers_db`.`burgers`;
-SELECT * FROM `burgers_db`.`burgers` LIMIT 1000;
-INSERT INTO `burgers_db`.`burgers` (`burger_name`) VALUES ('Rodeo Burger');
-SELECT LAST_INSERT_ID();
-SELECT `id`, `burger_name`, `devoured` FROM `burgers_db`.`burgers` WHERE  `id`=1;
-INSERT INTO `burgers_db`.`burgers` (`burger_name`) VALUES ('Vegan Burger');
-SELECT LAST_INSERT_ID();
-SELECT `id`, `burger_name`, `devoured` FROM `burgers_db`.`burgers` WHERE  `id`=2;
-INSERT INTO `burgers_db`.`burgers` (`burger_name`) VALUES ('Billy Burger');
-SELECT LAST_INSERT_ID();
-SELECT `id`, `burger_name`, `devoured` FROM `burgers_db`.`burgers` WHERE  `id`=3;
+
+INSERT INTO burgers (burger_name) VALUES ('Sylvester');
+INSERT INTO burgers (burger_name) VALUES ('Marilyn Manson');
+INSERT INTO burgers (burger_name) VALUES ('Joe Biden');
+
