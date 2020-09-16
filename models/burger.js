6f01461d7afb6ddba165dc63d38ff83efa2b8819
Chad Laflamme <cjlaflamme1@plymouth.edu>
2020-09-15 21:05:09 -0400

    instance of DAL class ,burger controller created

--- models/burger.js
+++ models/burger.js
@@ -1,3 +1,16 @@
-// import orm
-// Create code that will call the DAL functions using burger specific input for the DAL.
-// Don't forget to export!
+const DAL = require('../config/dbManager');
+
+const burger = {
+    selectAll: function() {
+        return DAL.selectAll("burgers");
+    }, 
+    insertOne: function(column, value) {
+        return DAL.insertOne("burgers", column, value);
+    }, 
+    updateOne: function(newValues, condition) {
+        return DAL.updateOne("burgers", newValues, condition);
+    },
+    delete: function(condition) {
+        return DAL.delete("burgers", condition);
+    }
+}
