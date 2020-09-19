be85bad6896a8a43694a40ee5c26bbdf69170619
Chad Laflamme <cjlaflamme1@plymouth.edu>
2020-09-18 23:23:07 -0400

    modified seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,5 +1,5 @@
 
-INSERT INTO burgers (burger_name) VALUES ('Sylvester');
-INSERT INTO burgers (burger_name) VALUES ('Marilyn Manson');
-INSERT INTO burgers (burger_name) VALUES ('Joe Biden');
+INSERT INTO burgers (burger_name) VALUES ('Big Taco Burger');
+INSERT INTO burgers (burger_name) VALUES ('Avacado Burger');
+INSERT INTO burgers (burger_name) VALUES ('Big Baby Bubba');
 
